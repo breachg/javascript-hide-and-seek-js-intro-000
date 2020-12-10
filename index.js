524abf3c8f241acd64ede1756701a43486a2d9c7
Garrett Breach <breachg1992@gmail.com>
2020-12-10 20:53:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  let target = document.querySelector('div#nested div div div div')
+  return target
+}
+
+function increaseRankBy(n) {
+  let lis = document.querySelectorAll('ul.ranked-list li');
+
+  for (let i =0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
